166c9c4eb6dc1f64cb864ec7154d875f692e760f
Yazzid388 <cikibersmkjaya@gmail.com>
2025-03-14 14:30:45 +0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: ['index.html'],
+  darkMode: 'class',
+  theme: {
+    extend: {
+      colors: {
+        primary: '#11151c',  
+        secondary: '#7776bc',
+        tertiary: '#e7e7e7',
+        dark: '#0f172a',
+      },
+      container: {
+        center: true,
+        padding: '16px',
+      },
+      mode: "jit",
+      screens: {
+        '2xl': '1320px',
+      },
+    },
+  },
+  plugins: [],
+};
+
+
